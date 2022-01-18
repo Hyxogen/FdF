a1fbbab27dfd299ef60364a2e10effa0fdc2bf47
Daan Meijer <daansander02@outlook.com>
2022-01-18 14:59:26 +0100

    Remove unused functions

--- src/math/vector3f.h
+++ src/math/vector3f.h
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/18 12:54:35 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/18 14:51:54 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/18 14:59:13 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,4 @@ typedef struct s_vector3f {
 	float	m_z;
 }	t_vector3f;
 
-t_vector3f	vector3f_add(const t_vector3f *a, const t_vector3f *b);
-
-t_vector3f	vector3f_muls(const t_vector3f *a, float scalar);
-
-float		vector3f_dot(const t_vector3f *a, const t_vector3f *b);
 #endif
