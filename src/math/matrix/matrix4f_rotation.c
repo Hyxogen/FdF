b5eb7847606e9e3cc8e6ae45be36dda6997bf6d6
Daan Meijer <daansander02@outlook.com>
2022-01-18 14:48:48 +0100

    Add rotation matrix

--- src/math/matrix/matrix4f_rotation.c
+++ src/math/matrix/matrix4f_rotation.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/18 12:55:39 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/18 14:39:15 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/18 14:47:33 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ t_matrix4f
 	t_matrix4f	out;
 
 	ft_memset(&out, 0, sizeof(t_matrix4f));
+	out.m_elements[0 + 0 * 4] = axis->m_x * axis->m_x * omc + c;
+	out.m_elements[0 + 1 * 4] = axis->m_x * axis->m_y * omc + axis->m_z * s;
+	out.m_elements[0 + 2 * 4] = axis->m_x * axis->m_z * omc + axis->m_y * s;
+	out.m_elements[1 + 0 * 4] = axis->m_x * axis->m_y * omc - axis->m_z * s;
+	out.m_elements[1 + 1 * 4] = axis->m_y * axis->m_y * omc + c;
+	out.m_elements[1 + 2 * 4] = axis->m_y * axis->m_z * omc + axis->m_x * s;
+	out.m_elements[2 + 0 * 4] = axis->m_x * axis->m_z * omc + axis->m_y * s;
+	out.m_elements[2 + 1 * 4] = axis->m_y * axis->m_z * omc + axis->m_x * s;
+	out.m_elements[2 + 2 * 4] = axis->m_z * axis->m_z * omc + c;
 	return (out);
 }
 
@@ -33,4 +42,5 @@ t_matrix4f
 	s = sin(angle);
 	c = cos(angle);
 	omc = 1.0f - c;
+	return (_matrix4f_rotation(axis, s, c, omc));
 }
