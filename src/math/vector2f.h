5af8b74d743ff174934dddc44d05fcad4b944d95
Daan Meijer <daansander02@outlook.com>
2022-01-19 10:00:53 +0100

    Add vector2f

---
+++ src/math/vector2f.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   vector2f.h                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/19 09:58:23 by dmeijer       #+#    #+#                 */
+/*   Updated: 2022/01/19 09:59:06 by dmeijer       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR2F_H
+# define VECTOR2F_H
+
+# include "../util/types.h"
+
+typedef struct s_vector2f {
+	t_fl32	m_x;
+	t_fl32	m_y;
+}	t_vector2f;
+
+#endif
