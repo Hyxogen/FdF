98fa93267999bd9a04be90e3285cddcaaff930da
Hyxogen <daansander02@outlook.com>
2022-01-21 09:20:17 +0100

    Switch back to matrix4f_clear

--- src/math/matrix4f/matrix4f_rotation.c
+++ src/math/matrix4f/matrix4f_rotation.c
@@ -19,7 +19,7 @@ t_matrix4f
 {
 	t_matrix4f	out;
 
-	matrix4f_make_identiy(&out);
+	matrix4f_clear(&out);
 	out.m_elements[0 + 0 * 4] = axis.m_x * axis.m_x * omc + c;
 	out.m_elements[0 + 1 * 4] = axis.m_x * axis.m_y * omc + axis.m_z * s;
 	out.m_elements[0 + 2 * 4] = axis.m_x * axis.m_z * omc + axis.m_y * s;
@@ -29,6 +29,7 @@ t_matrix4f
 	out.m_elements[2 + 0 * 4] = axis.m_x * axis.m_z * omc + axis.m_y * s;
 	out.m_elements[2 + 1 * 4] = axis.m_y * axis.m_z * omc + axis.m_x * s;
 	out.m_elements[2 + 2 * 4] = axis.m_z * axis.m_z * omc + c;
+	out.m_elements[3 + 3 * 4] = 1.0f;
 	return (out);
 }
 
